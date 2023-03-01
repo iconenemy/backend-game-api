8bc6c817149171d37de0566d4f26c5c225cd045a
iconenemy <iconenemy@gmail.com>
2023-03-01 14:21:20 +0200

    initialiation project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const bootstrap = async () => {
+  try {
+    const PORT = process.env.PORT
+
+    const appOptions = {cors: true}
+
+    const app = await NestFactory.create(AppModule, appOptions);
+    app.setGlobalPrefix('api')
+    
+    await app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 bootstrap();
